01adfa04f541df00ac7634b7e80ab9033cd96821
visatk <visatk29@gmail.com>
2026-05-24 19:17:13 +0600

    Delete worker/schema.ts

--- worker/schema.ts
+++
@@ -1,7 +0,0 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
-
-export const users = sqliteTable('users', {
-  id: integer('id').primaryKey({ autoIncrement: true }),
-  name: text('name').notNull(),
-  createdAt: integer('created_at', { mode: 'timestamp' }).notNull(),
-});
